f863e9d7288d697ae048f6ec5eda0041d3007b01
Yoel Hor <yoelhor@live.com>
2023-07-25 23:19:03 +0300

    Adding App Services logging

--- Program.cs
+++ Program.cs
@@ -2,8 +2,22 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Abstractions;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Logging.AzureAppServices;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Azure stream log service
+builder.Logging.AddAzureWebAppDiagnostics();
+builder.Services.Configure<AzureFileLoggerOptions>(options =>
+{
+    options.FileName = "azure-diagnostics-";
+    options.FileSizeLimit = 50 * 1024;
+    options.RetainedFileCountLimit = 5;
+});
+builder.Logging.AddFilter((provider, category, logLevel) =>
+{
+    return provider.ToLower().Contains("woodgroveapi");
+});
 
 ConfigurationSection WoodgroveGroceriesDownstreamApi = (ConfigurationSection)builder.Configuration.GetSection("WoodgroveGroceriesDownstreamApi");
 
